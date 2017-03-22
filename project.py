4bef74d1219178dec713f595432d3c42ae338e38
parth <parthchauhan05@gmail.com>
2017-03-22 13:08:25 +0530

    22 March

--- project.py
+++ project.py
@@ -5,14 +5,20 @@ def printtext():
 	print string 
 
 def process():
+	b.destroy()
+	e.grid(in_=window)
 	e.pack()
-	e.focus_set()
-	b = Button(root,text = "OK",command = printtext)
-	b.pack(side='bottom')
+	#e.focus_set()
+	b2 = Button(window,text = "OK",command = printtext)
+	b2.grid(in_=window)
+	b2.pack(side='bottom')
 
 root = Tk()
 root.title("Simalating Process Scheduling")
+window = Frame(root,width = 720, height = 480)
+window.pack(fill=BOTH, expand = YES)
 e = Entry()
 b = Button(root,text = "button", command = process)
+b.grid(in_=window)
 b.pack()
 root.mainloop()
